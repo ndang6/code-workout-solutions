fc16f7e741d330b591b910af9dbe1e1cd34ca8bc
ndang6 <ndang6@gmu.edu>
2020-10-19 10:22:10 -0700

    Decode Ways

--- trapping-rain-water.java
+++ trapping-rain-water.java
@@ -1,22 +1,22 @@
 class Solution {
-    public int trap(int[] input) {
+    public int trap(int[] height) {
         int max = 0;
         
         int i = 0;
-        int j = input.length - 1;        
+        int j = height.length - 1;        
         int leftmax = 0;
         int rightmax = 0;
         
         while(i <= j){
-            leftmax = Math.max(leftmax, input[i]);
-            rightmax = Math.max(rightmax, input[j]);
+            leftmax = Math.max(leftmax, height[i]);
+            rightmax = Math.max(rightmax, height[j]);
             
             if(leftmax < rightmax){
-                max += leftmax - input[i];
+                max += leftmax - height[i];
                 i++;
             }
             else{
-                max+= rightmax - input[j];
+                max+= rightmax - height[j];
                 j--;
             }
         }
