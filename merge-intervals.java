ac337aa5098a5219c5d92205dff6c733d97e5bea
ndang6 <ndang6@gmu.edu>
2020-11-02 16:52:16 -0800

    merge intervals

---
+++ merge-intervals.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1)
+			return intervals;
+
+        Comparator<int[]> myComp = new Comparator<int[]>() {
+            @Override
+            public int compare(int[] i1, int[] i2) {
+                return i1[0] - i2[0];
+            }
+        };
+        Arrays.sort(intervals, myComp);
+        
+		List<int[]> list = new ArrayList<>();
+        
+		int[] pendingInterval = intervals[0];
+		list.add(pendingInterval);
+        
+		for (int[] currentInterval : intervals) {       
+            
+			if (pendingInterval[1] >= currentInterval[0])
+				pendingInterval[1] = Math.max(pendingInterval[1], currentInterval[1]);
+            
+			else {                             
+				pendingInterval = currentInterval;
+				list.add(pendingInterval);
+			}
+		}
+        
+        int size = list.size();
+		return list.toArray(new int[size][]);
+    }
+}
