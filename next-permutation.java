622b79dc1b2a8e6cc361dab236f69b56f0e8efd4
ndang6 <ndang6@gmu.edu>
2020-11-06 11:42:50 -0800

    next permutation

---
+++ next-permutation.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+      int n = nums.length - 1, p = -1, pv = 0;
+
+      // step 1, be careful, i starts from n - 1
+      for(int i = n - 1; i >= 0; i--){
+        if(nums[i] < nums[i + 1]) {
+            p = i;
+            pv = nums[i];
+            break;
+        }
+      }    
+      
+      if(p == -1) {
+        reverse(nums, 0, n);
+        return;
+      }
+      
+      // step 2
+      for(int i = n; i >= 0; i--){
+        if(nums[i] > pv){
+          swap(nums, p, i);
+          break;
+        }
+      }
+      
+        
+      // step 3
+      reverse(nums, p + 1, n);
+    }
+    
+    private void reverse(int[] nums, int s, int e){
+      while(s < e){
+        swap(nums, s++, e--);
+      }
+    }
+    
+    private void swap(int[] nums, int s, int e){
+        int t = nums[s];
+        nums[s] = nums[e];
+        nums[e] = t;     
+    }
+}
