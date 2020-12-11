8c8ec423f94e83edfb4813f885514966abc08bbf
ndang6 <ndang6@gmu.edu>
2020-12-11 10:52:22 -0800

    Updated decode string

--- decode-string.java
+++ decode-string.java
@@ -1,42 +1,47 @@
+// s = 3[a]2[bc]
+// returns aaabcbc
 class Solution {
-    public String decodeString(String s) {
-        String res = "";
-        Stack<Integer> countStack = new Stack<>();
-        Stack<String> resStack = new Stack<>();
-        int idx = 0;
-        
-        while (idx < s.length()) {
-            
-            if (Character.isDigit(s.charAt(idx))) {
-                int count = 0;
-                while (Character.isDigit(s.charAt(idx))) {
-                    count = 10 * count + (s.charAt(idx) - '0');
-                    idx++;
-                }
-                countStack.push(count);
-            }
-            
-            else if (s.charAt(idx) == '[') {
-                resStack.push(res);
-                res = "";
-                idx++;
-            }
-            
-            else if (s.charAt(idx) == ']') {
-                StringBuilder temp = new StringBuilder (resStack.pop());
-                int repeatTimes = countStack.pop();
-                for (int i = 0; i < repeatTimes; i++) {
-                    temp.append(res);
-                }
-                res = temp.toString();
-                idx++;
-            }
-            
-            else 
-                res += s.charAt(idx++);
-            
+	public String decodeString(String s){
+		String res = "";
+      
+      Stack<Integer> countStack = new Stack<>();
+      Stack<String> stringStack = new Stack<>();
+      int index = 0;
+      
+      while(index < s.length()){
+        /* Step 1: Count the digit */
+        if(Character.isDigit(s.charAt(index))){
+          int count = 0;
+          while(Character.isDigit(s.charAt(index))){
+            count = count * 10 + (s.charAt(index++) - '0');
+          }
+          countStack.push(count);
         }
         
-        return res;
-    }
+        else if(s.charAt(index) == '['){
+          stringStack.push(res); // save the current result
+          res = "";              // reset to empty string
+          index = index + 1;
+        }
+        
+        else if(s.charAt(index) == ']'){
+          StringBuilder temp = new StringBuilder(stringStack.pop()); // get the previous result string
+          int repeatTimes = countStack.pop();
+          
+          for(int i = 0; i < repeatTimes; i++){
+            temp.append(res); /* Step 3: appending */
+          }
+          
+          res = temp.toString();
+          index = index + 1;
+        }
+        
+        /* Step 2: Count the characters */
+        else{
+          res += s.charAt(index++);
+        }
+      }
+      
+      return res;
+	}
 }
