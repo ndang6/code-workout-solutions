ec643df2470aa6f47af4ad07962c6609f376ed0a
ndang6 <60610186+ndang6@users.noreply.github.com>
2021-04-26 15:46:50 -0400

    Update subarray-sum-equals-k.java

--- subarray-sum-equals-k.java
+++ subarray-sum-equals-k.java
@@ -1,5 +1,7 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        // Optimal solution: https://leetcode.com/problems/subarray-sum-equals-k/discuss/102106/Java-Solution-PreSum-%2B-HashMap
+        
         int count = 0;
         
         for (int start = 0; start < nums.length; start++) {
