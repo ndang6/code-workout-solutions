3a94c5f4f619c5abb2c4764b84eae4a0aad39f40
ndang6 <dangtrankhoinguyen@gmail.com>
2021-05-15 11:00:30 -0400

    Update merge-intervals.java

--- merge-intervals.java
+++ merge-intervals.java
@@ -1,32 +1,23 @@
+// intervals = [[1,4],[2,3],[15,18],[8,10]]
+// returns [[1,4],[8,10],[15,18]]
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        if (intervals.length <= 1)
-			return intervals;
+	public int[][] merge(int[][] intervals) {
+		Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] i1, int[] i2) {
-                return i1[0] - i2[0];
-            }
-        });
-        
 		List<int[]> list = new ArrayList<>();
-        
 		int[] pendingInterval = intervals[0];
 		list.add(pendingInterval);
-        
-		for (int[] currentInterval : intervals) {       
-            
-			if (pendingInterval[1] >= currentInterval[0])
-				pendingInterval[1] = Math.max(pendingInterval[1], currentInterval[1]);
-            
-			else {                             
-				pendingInterval = currentInterval;
+
+		for (int[] interval : intervals) {
+			if (pendingInterval[1] >= interval[0]) {
+				pendingInterval[1] = Math.max(pendingInterval[1], interval[1]);
+			} else {
+				pendingInterval = interval;
 				list.add(pendingInterval);
 			}
 		}
-        
-        int size = list.size();
+
+		int size = list.size();
 		return list.toArray(new int[size][]);
-    }
+	}
 }
